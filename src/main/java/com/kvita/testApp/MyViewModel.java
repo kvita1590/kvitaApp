a092281d2770d58a31590bb02c34d2f99bd21ebd
Kvita Gennadiy <i@kvita.com.ua>
2013-02-26 05:44:45 +0200

    First commit

---
+++ src/main/java/com/kvita/testApp/MyViewModel.java
@@ -0,0 +1,17 @@
+package com.kvita.testApp;
+
+import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.Init;
+
+public class MyViewModel {
+
+	@Init
+	public void init() {
+
+	}
+
+	@Command
+	public void cmd() {
+
+	}
+}
